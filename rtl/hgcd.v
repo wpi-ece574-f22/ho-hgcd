b07a851d9788f930f83bbd9f33e54a3ab731fa11
Patrick Schaumont <pschaumont@wpi.edu>
2022-08-31 01:12:43 +0000

    upload

---
+++ rtl/hgcd.v
@@ -0,0 +1,12 @@
+module hgcd(
+    input wire[7:0] a,
+    input wire[7:0] b,
+    input wire ld,
+    input wire clk,
+    input wire reset,
+    output wire[7:0] q,
+    output wire rdy);
+
+   // design the RTL of this module ..
+
+endmodule
